f76d2e9d87f3f183d21a0085fb368f071bc8caed
Busayo Akeredolu <olubusayoakeredolu@gmail.com>
2020-02-06 14:04:01 +0000

    Started Sentence and Text Analysis

--- main.py
+++ main.py
@@ -12,25 +12,9 @@ def converse(userInput):
         print(userInput)
         conversation = False
 
-        # for i in range(len(words)):
-        #     words[i] = str.lower(words[i])
-        # if words.__contains__("hello") or words.__contains__("hi"):
-        #     print("Hello")
-        # elif words.__contains__("sad"):
-        #     print("That's too bad")
-        # elif words.__contains__("where"):
-        #     print("Up North")
-        # elif words.__contains__("?"):
-        #     print("Ask me again some other time")
-        # elif words.__contains__("bye") or words.__contains__("goodbye"):
-        #     print("I have to leave now")
-        #     conversation = False
-        # else:
-        #     print("I'm afraid I can't respond to that")
+
     return output
-# def getTokens(userInput):
-#     words = stemmer.tokenize(userInput)
-#     return words
+
 
 userInput = input("")
 converse(userInput)
@@ -38,7 +22,3 @@ converse(userInput)
 
 
 # NLTK: Bird, Steven, Edward Loper and Ewan Klein (2009), Natural Language Processing with Python. O’Reilly Media Inc.
-''' STANFORD NLP: Peng Qi, Timothy Dozat, Yuhao Zhang and Christopher D. Manning. 2018. 
-Universal Dependency Parsing from Scratch In Proceedings of the CoNLL 2018 Shared Task: 
-Multilingual Parsing from Raw Text to Universal Dependencies, pp. 160-170. [pdf] [bib] '''
-
--- tagging.py
+++ tagging.py
@@ -1,39 +1,15 @@
 #!/usr/local/bin/python3
+import nltk, csv
 
-import nltk, stanfordnlp
-from nltk.text import Text
-import stemming as stemmer
-from nltk.tokenize import sent_tokenize, PunktSentenceTokenizer
-from nltk.corpus import webtext as web
-from nltk.tag import StanfordPOSTagger, StanfordNERTagger
-# from pickle import load, dump
-
-monty = nltk.sent_tokenize(web.raw('grail.txt'))
-# nlp = stanfordnlp.Pipeline(processors='tokenize,mwt,pos')
-# tagList = []
-# for i in range(len(monty)):
-#     doc = nlp(monty[i])
-#     # print(*[f'word: {word.text+" "}\tupos: {word.upos}\txpos: {word.xpos}' for sent in doc.sentences for word in sent.words], sep='\n')
-#     tags = {word.text:word.upos for sent in doc._sentences for word in sent.words}
-#     tagList.append(tags)
-
-# [print(tagList[i], end = '\n') for i in range(len(tagList))]
-
-jar = 'stanford-postagger-2018-10-16/stanford-postagger.jar'
-model = 'stanford-postagger-2018-10-16/models/english-left3words-distsim.tagger'
-
-spt = StanfordPOSTagger(model, jar, encoding='utf8')
-nrt = StanfordNERTagger(model, jar, encoding='utf8')
-
+with open("MontyPython.txt") as file:
+    lines = file.readlines()
+lines = [line.strip() for line in lines] 
+lines = [nltk.sent_tokenize(lines[i]) for i in range(len(lines))]
+lines = [nltk.word_tokenize(str(lines[i])) for i in range(len(lines))]
 
 def assignTags(items):
-    response = ""
     for i in range(len(items)):
-        text = nltk.word_tokenize(items[i])
-        items[i] = nltk.pos_tag(text, tagset="universal")
-        response + str(items[i])
-        # print(line)
-    return response
+        items[i] = nltk.pos_tag(items[i])
 
 def evaluateTags(testList, trainingList):	
     t0 = nltk.DefaultTagger("NN")
@@ -41,11 +17,6 @@ def evaluateTags(testList, trainingList):
     t2 = nltk.BigramTagger(trainingList, backoff=t1)
     t3 = nltk.TrigramTagger(trainingList, backoff=t2)
 
-    # For storing trained tags on large pieces of text
-    # output = open('t2.pkl', 'wb')
-    # dump(t2, output, -1)
-    # output.close()
-    
     if t3.evaluate(testList) > 0.7:
         return testList
     else:
@@ -55,27 +26,62 @@ def printTags(items):
     for i in range(len(items)):
         print(items[i], end="")
 
-assignTags(monty)
+# assignTags(lines)
+# [print(lines[i], end='\n') for i in range(len(lines))]
 
-trainingSize = int(len(monty) * 0.7)
-trainingSents = monty[:trainingSize]
-testSents = monty[trainingSize:]
+trainingSize = int(len(lines) * 0.7)
+trainingSents = lines[:trainingSize]
+testSents = lines[trainingSize:]
 
-evaluateTags(testSents, trainingSents)
+# evaluateTags(testSents, trainingSents)
+
+"""SENTIMENT ANALYSIS"""
+
+sentenceTypes = [1, 2, 3, 4] # 1- Simple/Declarative, 2 - Imperative, 3 - Interrogative, 4 - Exclamatory 5 - Sound Effects, 6 - Descriptions, 7 - Non-English
+sentimentTags = [1, 2, 3, 4, 5, 6] # 1 - Negative, 2 - Somewhat Negative, 3 - Sarcastic, 4 - Neutral, 5 - Somewhat positive, 6 - Positive
+
+def writeToFile(listToWrite, fileName):
+    with open(fileName, 'w', newline='') as f:
+        writer = csv.writer(f)
+        writer.writerow(["PhraseID", "SentenceID", "Sentence"])
+        for i in range(len(listToWrite)):
+            writer.writerow([i, i, listToWrite[i]])
+
+# writeToFile(trainingSents, 'trainingList.csv')
+# writeToFile(testSents, 'testList.csv')
 
 
-# print(evaluateTags(trainingSents, testSents)) # = 0.8096951735817104
-# print(testSents)
-# print(evaluateTags(nltk.word_tokenize("What is the airspeed of an unladen swallow?")))
+
+
+"""WORKING WITH GRAMMARS"""
+sentence = [("the", "DT"), ("little", "JJ"), ("yellow", "JJ"), ("dog", "NN"), ("barked", "VBD"), ("at", "IN"),  ("the", "DT"), ("cat", "NN")]
+
+# def chunkSentence(inputSentence):
+#     grammar = "NP: {<DT>?<JJ>*<NN>}" 
+#     cp = nltk.RegexpParser(grammar) 
+#     result = cp.parse(inputSentence) 
+#     print(result) 
+
+# # (S
+# #   (NP the/DT little/JJ yellow/JJ dog/NN)
+# #   barked/VBD
+# #   at/IN
+# #   (NP the/DT cat/NN))
+# result.draw() 
+
+grammar1 = nltk.data.load('file:grammars.cfg')
+sent = "Mary saw Bob".split()
+rd_parser = nltk.RecursiveDescentParser(grammar1)
+for tree in rd_parser.parse(sent):
+    print(tree)
 
 
 
 
-# ADJ: adjective, ADP: adposition, ADV: adverb, AUX: auxiliary, CCONJ: coordinating conjunction, DET: determiner
-# INTJ: interjection, NOUN: noun, NUM: numeral, PART: particle, PRON: pronoun, PROPN: proper noun, PUNCT: punctuation 
-# SCONJ: subordinating conjunction, SYM: symbol, VERB: verb, X: other
-# © 2014–2017 Universal Dependencies contributors. Site powered by Annodoc and brat
-# Source: https://universaldependencies.org/u/pos/
+
+
+
+
 
 # NLTK: Bird, Steven, Edward Loper and Ewan Klein (2009), Natural Language Processing with Python. O’Reilly Media Inc.
 ''' STANFORD NLP: Peng Qi, Timothy Dozat, Yuhao Zhang and Christopher D. Manning. 2018. 
